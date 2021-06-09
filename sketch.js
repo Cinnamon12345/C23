6d4c9e4f659cd5e7c6f1878633f7009662e55885
Cinnamon12345 <84158052+Cinnamon12345@users.noreply.github.com>
2021-06-09 15:51:27 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var box2;
+var ground;
+
+function setup(){
+    var canvas = createCanvas(800,400);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(400, 390, 800, 20);
+    box1 = new Box(200,100,50,50);
+    box2 = new Box(150, 50, 100,50 );
+    
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground.display();
+   
+}
